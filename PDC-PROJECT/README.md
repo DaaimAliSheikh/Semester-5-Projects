2bbef2e20c87445698f220242e6a6c7fb3803c15
DaaimAli <daaimalisheikh23@gmail.com>
2024-09-18 04:08:33 +0500

    pdc synchronous code added

--- PDC-PROJECT/README.md
+++ PDC-PROJECT/README.md
@@ -3,6 +3,14 @@ COMMANDS TO RUN when in the same directory as the MakeFile:
 -make run //run the project, or build and run if the project has not been built yet
 -make clean //delete build and bin folders
 
+PARALLELIZATION TO DO:
+
+- parallelize the tokenization of each sentence in sentences(main.cpp)
+- parallelize TFIDF generation of each document(main.cpp)
+- parallelize file reading(getSentences.cpp)
+- parallelize cosine_similarity search of inputTFIDF with each of the otehr IDFs in the for loop(main.cpp)
+- try to parallelize the calculateIDF logarithm loop, divide the docCount array into chunks(calculateIDF.cpp)
+
 FOLDER STRUCTURE
 project/
 ├── src/
@@ -13,5 +21,6 @@ project/
 │ └── module2/
 │ ├── file2.cpp
 │ ├── file2.h
+|── include/ #Directory for header files
 ├── build/ # Directory for object files
 └── bin/ # Directory for the final executable
--- PDC-PROJECT/src/main.cpp
+++ PDC-PROJECT/src/main.cpp
@@ -1,7 +1,66 @@
+#include "calculateIDF.hpp"
+#include "calculateTFIDF.hpp"
+#include "findTopKMostSimilarDocs.hpp"
+#include "getSentences.hpp"
+#include "tokenize.hpp"
+#include <fstream>
 #include <iostream>
-using namespace std;
+#include <map>
+#include <sstream>
+#include <vector>
 
 int main() {
-  cout << "Hello World!" << endl;
+
+  // Path should be relative to Makefile location
+  std::cout << "Processing file: test.txt........" << std::endl;
+
+  std::string filepath = "document_files/test.txt";
+
+  std::vector<std::string> sentences = getSentences(filepath);
+
+  std::vector<std::vector<std::string>> documents;
+  for (const auto &sentence : sentences)
+    documents.push_back(tokenize(sentence));
+
+  // Calculate IDF from all documents
+  std::map<std::string, double> idf = calculateIDF(documents);
+
+  // Calculate TF-IDF for all documents
+  std::vector<std::map<std::string, double>> docTFIDFs;
+  for (const auto &doc : documents) {
+    docTFIDFs.push_back(calculateTFIDF(doc, idf));
+  }
+  // for (const auto &pairing : idf){
+  //   std::cout << pairing.first   << std::endl;
+  // }
+  // for (const auto &sentence : sentences){
+  //   std::cout << sentence  <<"END"<< std::endl;
+  // }
+
+  std::string question;
+  std::cout << "Ask a question regarding the dataset you provided:"
+            << std::endl;
+  std::getline(std::cin, question);
+
+  int k;
+  std::cout << "how many top matching sentences would you like to see?"
+            << std::endl;
+  std::cin >> k;
+
+  std::map<std::string, double> inputTFIDF =
+      calculateTFIDF(tokenize(question), idf);
+
+  std::vector<int> topIndices =
+      findTopKMostSimilarDocs(inputTFIDF, docTFIDFs, k);
+
+  
+ std::cout<<"size"<<topIndices.size()<<std::endl;
+  std::cout << "The top " << k << " most similar sentences are:" <<
+  std::endl;
+
+  for (int i = 0; i < topIndices.size(); ++i) {
+    std::cout << i + 1 << ". " << sentences[topIndices[i]] << std::endl;
+  }
+
   return 0;
 }
