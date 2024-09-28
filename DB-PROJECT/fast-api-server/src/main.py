d17147b9a307797456c4103a5a7415f22e0b1691
DaaimAli <daaimalisheikh23@gmail.com>
2024-09-28 17:40:28 +0500

    changed .gitignore

--- DB-PROJECT/fast-api-server/src/main.py
+++ DB-PROJECT/fast-api-server/src/main.py
@@ -7,11 +7,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Replace with your React app's URL
-    allow_credentials=True, #allow client to send cookies
+    allow_credentials=True,  # allow client to send cookies
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
 )
 
+
 @app.get("/")
 async def root():
-    return "hello"
+    return "hello bruhh"
